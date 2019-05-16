61f91450dd5554c243203a159cf6acb432b8e529
Patrick K. Moffitt <Patrick@Moffitt.com>
2019-05-16 10:23:32 -0400

    Simplified loop() logic (cyclomatic complexity). Reduced run time for failed WiFi and MQTT failures saves battery. Increased run time for display use.

--- src/main.cpp
+++ src/main.cpp
@@ -37,7 +37,7 @@
 #include "serial_debug_error.hpp"
 #include <RTCZero.h>
 
-#define LOOP_LIMIT 6
+#define LOOP_LIMIT 4
 
 #define SLEEP_INTERVAL_SECONDS 900
 
@@ -75,14 +75,13 @@ Adafruit_INA219 ina219;
 volatile bool display_data{false};  // Button A toggles the display
 volatile bool degrees_c_f{false};   // Button C toggles the temperature scale.
 volatile bool system_time_set{false};
-volatile int loop_counter{1};
+volatile int loop_counter{0};
+std::bitset<5> publish_status{0};
 
 void alarm_handler();  // Advance declaration
 volatile bool alarm_set{false};
 
 void monitor_deep_sleep();  //  Advance declaration.
-
-int wifi_connection_attempts{0};
 
 void setup() {
 #if defined(SERIAL_DEBUG) || defined(SERIAL_ERROR)
@@ -103,7 +102,7 @@ void setup() {
     pinMode(BUTTON_B, INPUT_PULLUP);
     attachInterrupt(
             digitalPinToInterrupt(BUTTON_B),
-            [](){oled.page == 2 ? (oled.page = 0) : (oled.page++);},
+            [](){oled.page == 2 ? (oled.page = 0) : (oled.page++); loop_counter = 1;},
             FALLING
             );
     pinMode(BUTTON_C, INPUT_PULLUP);
@@ -119,124 +118,118 @@ void setup() {
 }
 
 void loop() {
+    loop_counter++;
+    DEBUG_PRINT("Loop "); DEBUG_PRINT(loop_counter); DEBUG_PRINT(" of ");
+    DEBUG_PRINTLN(LOOP_LIMIT);
+
     if (alarm_set == true) {
         alarm_set = false;
         oled.enable();
         WiFi.begin(WIFI_SSID, WIFI_PASS);
-
+        delay(500);
+        return;
     }
 
-    if (WiFi.status() == WL_CONNECTED) {
-        DEBUG_PRINTLN(ipv4_int_to_str(WiFi.localIP()));
+    if (loop_counter == LOOP_LIMIT) {
+        monitor_deep_sleep();
+    }
 
-        time_util.set_time_of_day();
+    if (publish_status.any() and not display_data) {
+        monitor_deep_sleep();
+    }
 
-        DEBUG_PRINT("NTP: "); DEBUG_PRINTLN(time_util.unix_epoch_time_gmt);
-        DEBUG_PRINT("Sensor Time: "); DEBUG_PRINTLN(sensor.unix_epoch_time);
+    if (WiFi.status() != WL_CONNECTED) {
+        uint8_t mac_address[6];
+        WiFi.macAddress(mac_address);
+        DEBUG_PRINT("WiFi is not connected. ");
+        DEBUG_PRINT("MAC Address: "); DEBUG_PRINTLN(macv4_int_to_str(mac_address));
+        delay(500 * loop_counter);
+        return;
+    }
+    DEBUG_PRINT("WiFi Connected. IP: "); DEBUG_PRINTLN(ipv4_int_to_str(WiFi.localIP()));
 
+    if (not mqtt.connected()) {
+        mqtt.connect();
+        delay(500 * loop_counter);
         if (not mqtt.connected()) {
-            mqtt.connect();
-            delay(100);
+            DEBUG_PRINTLN("Adafruit.io MQTT connection failure.");
+            return;
         }
-
-        sensor.battery_vdc = get_battery_percent();
-        sensor.current_ma = ina219.getCurrent_mA();
-        if (std::isnan(sensor.current_ma)) {
-            DEBUG_PRINTLN("Error reading current sensor!");
-        } else {
-            DEBUG_PRINT("Current : ");
-            DEBUG_PRINT(sensor.current_ma);
-            DEBUG_PRINTLN("mA");
-        }
-
-        sensors_event_t event;
-        dht.temperature().getEvent(&event);
-        if (std::isnan(event.temperature)) {
-            DEBUG_PRINTLN("Error reading temperature!");
-        } else {
-            sensor.temperature_f = event.temperature * 1.8 + 32;
-            DEBUG_PRINT("Temperature: ");
-            DEBUG_PRINT(sensor.temperature_f);
-            DEBUG_PRINTLN(" ℉");
-        };
-
-        dht.humidity().getEvent(&event);
-        if (std::isnan(event.relative_humidity)) {
-            DEBUG_PRINTLN("Error reading humidity!");
-        } else {
-            sensor.humidity_rh = event.relative_humidity;
-            DEBUG_PRINT("Relative Humidity: ");
-            DEBUG_PRINT(sensor.humidity_rh);
-            DEBUG_PRINTLN(" ϕ");
-        }
-
-        char payload_float[AIO_MQTT_PAYLOAD_FLOAT_MAX_SIZE];  // Float max 39 digits and minus sign.
-        char zero{'\0'};
-        std::bitset<5> publish_status{0};
-        if (mqtt.connected()) {
-            // Publish Battery VDC Percent
-            format::to_fixed_width(sensor.battery_vdc, ZERO_FIELD_PAD, payload_float);
-            publish_status[0] = mqtt.publish(BATTERY_VDC, payload_float, 0);
-            memcpy(&payload_float, &zero, AIO_MQTT_PAYLOAD_FLOAT_MAX_SIZE);
-
-            // Publish Current mA
-            format::to_fixed_width(sensor.current_ma, ZERO_FIELD_PAD, AIO_FLOAT_PRECISION, payload_float);
-            publish_status[1] = mqtt.publish(CURRENT_MA, payload_float, 0);
-            memcpy(&payload_float, &zero, AIO_MQTT_PAYLOAD_FLOAT_MAX_SIZE);
-
-            // Publish Humidity ϕ
-            format::to_fixed_width(sensor.humidity_rh, ZERO_FIELD_PAD, AIO_FLOAT_PRECISION, payload_float);
-            publish_status[2] = mqtt.publish(HUMIDITY_RH, payload_float, 0);
-            memcpy(&payload_float, &zero, AIO_MQTT_PAYLOAD_FLOAT_MAX_SIZE);
-
-            // Publish Temperature ℉
-            format::to_fixed_width(sensor.temperature_f, ZERO_FIELD_PAD, AIO_FLOAT_PRECISION, payload_float);
-            publish_status[3] = mqtt.publish(TEMPERATURE_F, payload_float, 0);
-            memcpy(&payload_float, &zero, AIO_MQTT_PAYLOAD_FLOAT_MAX_SIZE);
-
-            // Publish Unix Epoch Time UTC
-            publish_status[4] = mqtt.publish(UNIX_EPOCH_TIME, sensor.unix_epoch_time, 0);
-        }
-
-        DEBUG_PRINT("Loop "); DEBUG_PRINT(loop_counter); DEBUG_PRINT(" of ");
-        DEBUG_PRINTLN(LOOP_LIMIT);
-        if (display_data) {
-            oled.show_page(oled.page);
-            delay(5900);
-            loop_counter++;
-            if (loop_counter > LOOP_LIMIT) {
-                DEBUG_PRINTLN("Display work is exhausting. Tired now. Going to sleep. ZZZzzz...");
-                monitor_deep_sleep();
-            }
-        } else if (publish_status != 0) {  // At least one item was published.
-            delay(100);
-            DEBUG_PRINT("Publish status battery: ");
-            DEBUG_PRINTLN(publish_status[0]);
-            DEBUG_PRINT("Publish status current: ");
-            DEBUG_PRINTLN(publish_status[1]);
-            DEBUG_PRINT("Publish status humidity: ");
-            DEBUG_PRINTLN(publish_status[2]);
-            DEBUG_PRINT("Publish status temperature: ");
-            DEBUG_PRINTLN(publish_status[3]);
-            DEBUG_PRINT("Publish status time: ");
-            DEBUG_PRINTLN(publish_status[4]);
-            DEBUG_PRINTLN("No display work. Going back to sleep. ZZZzzz...");
-            monitor_deep_sleep();
-        }
-    } else {
-        uint8_t mac_address;
-        WiFi.macAddress(&mac_address);
-        DEBUG_PRINTLN("WiFi is not connected.");
-        DEBUG_PRINT("MAC Address: "); DEBUG_PRINTLN(macv4_int_to_str(&mac_address));
-        wifi_connection_attempts++;
-        DEBUG_PRINT("Connection attempt #");
-        DEBUG_PRINT(wifi_connection_attempts);
-        DEBUG_PRINTLN(" failed.");
-        if (wifi_connection_attempts == 10) {
-            monitor_deep_sleep();
-        }
-        delay(300);
     }
+    DEBUG_PRINT("MQTT Connection Status: "); DEBUG_PRINTLN(mqtt.connected());
+
+    sensor.battery_vdc = get_battery_percent();
+    sensor.current_ma = ina219.getCurrent_mA();
+    if (std::isnan(sensor.current_ma)) {
+        DEBUG_PRINTLN("Error reading current sensor!");
+        return;
+    }
+    DEBUG_PRINT("Current : "); DEBUG_PRINT(sensor.current_ma); DEBUG_PRINTLN("mA");
+
+    sensors_event_t event;
+    dht.temperature().getEvent(&event);
+    if (std::isnan(event.temperature)) {
+        DEBUG_PRINTLN("Error reading temperature!");
+        return;
+    }
+    sensor.temperature_f = event.temperature * 1.8 + 32;
+    DEBUG_PRINT("Temperature: "); DEBUG_PRINT(sensor.temperature_f); DEBUG_PRINTLN(" ℉");
+
+    dht.humidity().getEvent(&event);
+    if (std::isnan(event.relative_humidity)) {
+        DEBUG_PRINTLN("Error reading humidity!");
+        return;
+    }
+    sensor.humidity_rh = event.relative_humidity;
+    DEBUG_PRINT("Relative Humidity: "); DEBUG_PRINT(sensor.humidity_rh); DEBUG_PRINTLN(" ϕ");
+
+    if (display_data) {
+        oled.show_page(oled.page);
+        delay(5000);
+    }
+
+    char payload_float[AIO_MQTT_PAYLOAD_FLOAT_MAX_SIZE];  // Float max 39 digits and minus sign.
+    char zero{'\0'};
+
+    if (mqtt.connected()) {
+        // Publish Battery VDC Percent
+        format::to_fixed_width(sensor.battery_vdc, ZERO_FIELD_PAD, payload_float);
+        publish_status[0] = mqtt.publish(BATTERY_VDC, payload_float, 0);
+        memcpy(&payload_float, &zero, AIO_MQTT_PAYLOAD_FLOAT_MAX_SIZE);
+
+        // Publish Current mA
+        format::to_fixed_width(sensor.current_ma, ZERO_FIELD_PAD, AIO_FLOAT_PRECISION, payload_float);
+        publish_status[1] = mqtt.publish(CURRENT_MA, payload_float, 0);
+        memcpy(&payload_float, &zero, AIO_MQTT_PAYLOAD_FLOAT_MAX_SIZE);
+
+        // Publish Humidity ϕ
+        format::to_fixed_width(sensor.humidity_rh, ZERO_FIELD_PAD, AIO_FLOAT_PRECISION, payload_float);
+        publish_status[2] = mqtt.publish(HUMIDITY_RH, payload_float, 0);
+        memcpy(&payload_float, &zero, AIO_MQTT_PAYLOAD_FLOAT_MAX_SIZE);
+
+        // Publish Temperature ℉
+        format::to_fixed_width(sensor.temperature_f, ZERO_FIELD_PAD, AIO_FLOAT_PRECISION, payload_float);
+        publish_status[3] = mqtt.publish(TEMPERATURE_F, payload_float, 0);
+        memcpy(&payload_float, &zero, AIO_MQTT_PAYLOAD_FLOAT_MAX_SIZE);
+
+        // Publish Unix Epoch Time UTC
+        publish_status[4] = mqtt.publish(UNIX_EPOCH_TIME, sensor.unix_epoch_time, 0);
+        delay(100);
+    }
+
+    if (publish_status.any()) {  // At least one item was published.
+        DEBUG_PRINT("Publish status battery: ");
+        DEBUG_PRINTLN(publish_status[0]);
+        DEBUG_PRINT("Publish status current: ");
+        DEBUG_PRINTLN(publish_status[1]);
+        DEBUG_PRINT("Publish status humidity: ");
+        DEBUG_PRINTLN(publish_status[2]);
+        DEBUG_PRINT("Publish status temperature: ");
+        DEBUG_PRINTLN(publish_status[3]);
+        DEBUG_PRINT("Publish status time: ");
+        DEBUG_PRINTLN(publish_status[4]);
+    }
+
 }
 
 void alarm_handler() {
@@ -247,15 +240,21 @@ void alarm_handler() {
 void monitor_deep_sleep() {
     pinMode(PIN_LED_13, LOW);
     oled.disable();
+    display_data = false;
+    oled.page = 0;
     if (mqtt.connected()) {
         mqtt.disconnect();
     }
     client.stop();
     WiFi.end();
-    
-    auto alarm_time = time_util.unix_epoch_time_gmt + SLEEP_INTERVAL_SECONDS;
-    DEBUG_PRINT("RTC Epoch: "); DEBUG_PRINTLN(rtc.getEpoch());
+
+    auto epoch = rtc.getEpoch();
+    auto alarm_time = epoch + SLEEP_INTERVAL_SECONDS;
+
+    DEBUG_PRINTLN("Setting alarm and going to sleep.");
+    DEBUG_PRINT("RTC Epoch: "); DEBUG_PRINTLN(epoch);
     DEBUG_PRINT("RTC Alarm: "); DEBUG_PRINTLN(alarm_time);
+
     rtc.setAlarmEpoch((uint32) alarm_time);
     rtc.enableAlarm(rtc.MATCH_HHMMSS);
     rtc.standbyMode();
